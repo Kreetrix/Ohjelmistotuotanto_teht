33d88d25dd52f02f00abd7050ac37b75e612c9bc
kreetz <bronze.reach@gmail.com>
2025-09-04 14:46:07 +0300

    folders

--- assignments/src/main/java/ohjelmistotuotanto_tht/Main.java
+++
@@ -1,9 +0,0 @@
-package ohjelmistotuotanto_tht;
-
-public class Main {
-    public static void main(String[] args) {
-        TemperatureConverter converter = new TemperatureConverter();
-
-        System.out.println(converter.fahrenheitToCelsius(20.0));
-    }
-}
